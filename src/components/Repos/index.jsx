dd2b92d8e54feedbe994850b6c47861d5edfd495
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-25 15:25:23 +0300

    rewrote query for searching

--- src/components/Repos/index.jsx
+++ src/components/Repos/index.jsx
@@ -5,20 +5,25 @@ import { Spacer, Title } from "../../shared.styles";
 import CardList from "@components/CardList";
 import Filter from "@components/Filter";
 
-import { Owned as ALL_REPOS_QUERY } from "../../queries/repos.gql"
+// import { Owned as ALL_REPOS_QUERY } from "../../queries/repos.gql"
+import { Search as SEARCH_REPOS_QUERY } from "../../queries/repos.gql"
 
 const Repos = () => {
-  const { data, loading, error } = useQuery(ALL_REPOS_QUERY, {
+  const { data, loading, error } = useQuery(SEARCH_REPOS_QUERY, {
     variables: {
       // login: "airbnb"
-      login: "impraise"
+      // login: "impraise",
+      query: "org:impraise"
     }
   });
   
   if (loading) return <div>loading...</div>
   if (error) return <div>error...</div>
 
-  const items = data.organization.repositories.nodes
+  // console.log(data);
+
+  // const items = data.organization.repositories.nodes
+  const items = data.search.nodes;
 
   return (
     <React.Fragment>
